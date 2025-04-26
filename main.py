b2bc406dde177fccccff0d74abed09d782b151a2
Rico Metzger <rico.metzger@gmail.com>
2025-04-26 17:33:56 +0300

    finalizing with args

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words,get_num_characters,get_sorted_characters
+import sys
 
 def get_book_text(file_path):
   """
@@ -13,18 +14,23 @@ def get_book_text(file_path):
   with open(file_path, 'r', encoding='utf-8') as file:
     return file.read()
 
+def usage():
+  print("Usage: python3 main.py <path_to_book>")
+
 def main():
-  """
-  Just uses the other functions in this code to create an executable program.
-  """
-  book = "books/frankenstein.txt"
+  if len(sys.argv) != 2:
+    usage()
+    exit(1)
+
+  book = sys.argv[1]
   print("============ BOOKBOT ============")
   print(f"Analyzing book found at {book}...")
 
-  content = get_book_text("books/frankenstein.txt")
+  content = get_book_text(book)
   num_words = get_num_words(content)
   print("----------- Word Count ----------")
   print(f"Found {num_words} total words")
+
   num_characters = get_num_characters(content)
   sorted_characters = get_sorted_characters(num_characters)
   print("--------- Character Count -------")
